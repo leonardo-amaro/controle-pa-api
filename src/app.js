06544fa58e236f0a49c2c1e6b2488ec6da790aa5
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-04-16 10:35:22 -0300

    habilitando Cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import('@dotenvx/dotenvx/config');
 import express from "express";
+import cors from "cors";
 import routes from "./routes/index.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use("/", routes);
 
 const PORT = process.env.PORT || 8000;
